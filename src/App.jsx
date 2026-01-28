05f2e71795771ed8b0e447a763fd7a338e8ed41e
Alessandro Ledda <aleledda75@icloud.com>
2026-01-28 17:16:01 +0100

    feat collegamento pagine a app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+// import router
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// import delle pagine 
+import HomePage from './pages/HomePage'
+import AboutUs from './pages/AboutUs'
+import ListProducts from './pages/ListProducts'
+
+// import del layout(default)
+import DefaultLayout from './layout/DefaultLayout'
+
 
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/chisiamo" element={<AboutUs />} />
+            <Route path="/prodotti" element={<ListProducts />} />
+          </Route>
 
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
