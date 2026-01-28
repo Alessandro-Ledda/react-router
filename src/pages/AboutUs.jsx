badfaa8db1767b9fb8272a1dfcaa1219091774d0
Alessandro Ledda <aleledda75@icloud.com>
2026-01-28 17:27:51 +0100

    feat about us e home page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,12 @@
 function AboutUs() {
 
-    return ()
+    return (
+        <main>
+            <div className="container-about">
+                <h1>Ora ti trovi precisamente im "chi siamo" come puoi vedere dai contenuti</h1>
+            </div>
+        </main>
+    )
 }
 
 export default AboutUs
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,12 @@
 function HomePage() {
 
-    return ()
+    return (
+        <main>
+            <div className="container-main">
+                <h1 className="title"> Benvenuto nella Homepage</h1>
+            </div>
+        </main>
+    )
 }
 
 export default HomePage
