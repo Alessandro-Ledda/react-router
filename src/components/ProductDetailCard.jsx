02eb8491131c8d78a849170485acf9ccbe45b246
Alessandro Ledda <aleledda75@icloud.com>
2026-01-30 09:32:28 +0100

    style in file index.css

--- src/components/ProductDetailCard.jsx
+++ src/components/ProductDetailCard.jsx
@@ -7,18 +7,32 @@ function ProductDetailCard(props) {
     const { infoProd } = props
 
     return (
+
         <div className="wrap-details-card">
-            <h2 className="title">{infoProd.title}</h2>
-            <img src={infoProd.image} alt={infoProd.title} className="image" />
-            <h4 className="price">{infoProd.price}</h4>
-            <h3 className="category">{infoProd.category}</h3>
-            <h4 className="description">{infoProd.description}</h4>
-            <h4 className="rating-rate">{infoProd.rating.rate}</h4>
-            <h4 className="reting-count">{infoProd.rating.count}</h4>
-            <Link to="/prodotti">
-                Torna alla lista prodotti
-            </Link>
+
+            <h2 className="d-title">{infoProd.title}</h2>
+            <div className="det-content">
+                <div className="d-image">
+                    <img src={infoProd.image} alt={infoProd.title} />
+                </div>
+
+                <div className="others-det">
+                    <div className="item">
+                        <h4 className="d-price">{infoProd.price}</h4>
+                        <h3 className="d-category">{infoProd.category}</h3>
+                        <h4 className="d-description">{infoProd.description}</h4>
+                        <h4 className="d-rating-rate">{infoProd.rating.rate}</h4>
+                        <h4 className="d-reting-count">{infoProd.rating.count}</h4>
+                        <Link to="/prodotti">
+                            Torna alla lista prodotti
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
+
     )
 }
 
